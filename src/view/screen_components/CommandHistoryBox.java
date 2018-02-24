8ad70085a9462a4f5acb1a17904bbb4645ba61b3
Andy Nguyen <aln20@duke.edu>
2018-02-24 15:56:28 -0500

    finished my part of the structural chagnes

--- src/view/screen_components/CommandHistoryBox.java
+++ src/view/screen_components/CommandHistoryBox.java
@@ -1,21 +1,18 @@
 package view.screen_components;
 
 import controller.ControllerInterface;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import view.constants.ButtonConstants;
+import view.constants.LabelConstants;
+import view.constants.TextAreaConstants;
 
-public class CommandHistoryBox extends ScreenComponent{
-	private Button historyClearButton;
-	private TextArea historyTextArea;
-	private Label historyLabel;
+public class CommandHistoryBox extends HistoryBox{
 	public CommandHistoryBox(ControllerInterface controller){
 		super(controller);
 	}
 	public BorderPane generateGUIComponent(){
-		BorderPane borderPane = super.getBorderPane();
-		//TODO: create borderpane layout
-		return borderPane;
+		super.setDimensions(TextAreaConstants.HISTORY_ROWS, TextAreaConstants.HISTORY_COLUMNS);
+		super.setUpLabels(LabelConstants.HISTORY_LABEL_TEXT, ButtonConstants.HISTORY_BUTTON_LABEL);
+		return super.generateGUIComponent();
 	}
 }
