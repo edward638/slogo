cc92157316533f07881f8daf3d4c2d6b69bd9d58
Andy Nguyen <aln20@duke.edu>
2018-02-24 15:03:00 -0500

    added structure to ui chagnes again

--- src/view/screen_components/VariableHistoryBox.java
+++ src/view/screen_components/VariableHistoryBox.java
@@ -4,12 +4,18 @@ import controller.ControllerInterface;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 
-public class VariableHistoryBox extends ControllerHolder{
+public class VariableHistoryBox extends ScreenComponent{
 	private TextArea variableTextArea;
 	private Button variableClearButton;
 	private Label variableLabel;
 	public VariableHistoryBox(ControllerInterface controller){
 		super(controller);
+	}
+	public BorderPane generateGUIComponent(){
+		BorderPane borderPane = super.getBorderPane();
+		//TODO: create borderpane layout
+		return borderPane;
 	}
 }
