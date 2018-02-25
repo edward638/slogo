8201ffa85b516ab59168d65478f00d2ad2d0b969
Charles <cnd11@duke.edu>
2018-02-25 11:29:53 -0500

    Debugging movement mechanics

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -40,9 +40,11 @@ public class Turtle
 
 
 	public void setCoordinates(double xCoordinate, double yCoordinate) {
+		//System.out.println("old y" + YCoordinate);
 		Line l = new Line(XCoordinate, YCoordinate, xCoordinate, yCoordinate);
 		XCoordinate = xCoordinate;
 		YCoordinate = yCoordinate;
+		//System.out.println("new y" + YCoordinate);
 		addLine(l);
 	}
 
