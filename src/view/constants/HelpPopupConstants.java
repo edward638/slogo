b225ee1854148639985cff5bebfeb7304c7b13fc
Edward Zhuang <edward.zhuang@duke.edu>
2018-02-27 20:21:58 -0500

    Completed help screen

--- src/view/constants/HelpPopupConstants.java
+++ src/view/constants/HelpPopupConstants.java
@@ -1,5 +1,9 @@
 package view.constants;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
 public class HelpPopupConstants {
     public static final String BASIC_SYNTAX = "Basic Syntax";
     public static final String TURTLE_COMMANDS = "Turtle Commands";
@@ -8,8 +12,18 @@ public class HelpPopupConstants {
     public static final String BOOLEAN_OPERATIONS = "Boolean Operations";
     public static final String VARIABLES = "Variables/Structures";
     public static final String HELP_MESSAGE =
-            "Welcome to the SLogo help screen! Please click a button to learn more about" +
-            " how to use this program.";
+            "Welcome to the SLogo help screen! Please\nclick a button to learn more about" +
+            " how to\nuse this program.";
+    public static final String syntaxImage = "basic_syntax.png";
+    public static final String commandsImage = "turtle_commands.png";
+    public static final String queriesImage = "turtle_queries.png";
+    public static final String mathImage = "math_operations.png";
+    public static final String booleanImage = "boolean_operations.png";
+    public static final String variablesImage = "variables_structures.png";
+    public static final String helpTitle = "help_title.png";
+
+
+
 
     public static final int FONT_SIZE = 13;
 }
