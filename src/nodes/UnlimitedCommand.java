6127968d1d78271e2f187e9c5effbdceafa90c08
Charles <cnd11@duke.edu>
2018-03-05 22:15:21 -0500

    Unlimited command

---
+++ src/nodes/UnlimitedCommand.java
@@ -0,0 +1,68 @@
+package nodes;
+
+import Tree.TreeEvaluator;
+import Tree.TreeMaker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class UnlimitedCommand implements HeadInterface, NodeInterface {
+    private NodeInterface command;
+    private Stack children;
+    private double value;
+    private int numChildren;
+    TreeEvaluator te;
+
+    public UnlimitedCommand(NodeInterface command, int numChildren) {
+        this.command = command;
+        this.numChildren = numChildren;
+        children = new Stack();
+        value = 0.0;
+        te = new TreeEvaluator();
+    }
+
+    @Override
+    public NodeInterface getNext() {
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public double evaluate(List<NodeInterface> args) {
+        int childrenTracker = 0;
+        while (!children.empty()) {
+            ArrayList<NodeInterface> nodes = new ArrayList<>();
+            nodes.add(command);
+            while (!children.empty() && childrenTracker<numChildren) {
+                if (children.peek() instanceof Constant) {
+                    childrenTracker++;
+                }
+                nodes.add((NodeInterface) children.pop());
+            }
+            childrenTracker = 0;
+            TreeMaker tm = new TreeMaker(nodes);
+            value += te.evaluate( (ArrayList) tm.getHeads());
+        }
+        return value;
+    }
+
+    @Override
+    public void add(NodeInterface n) {
+        children.add(n);
+    }
+
+    @Override
+    public void reset() {
+        //do nothing
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+}
