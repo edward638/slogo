60f42a5c838baae68742ec2f01d27040fc646bd3
Belanie Nagiel <bn51@duke.edu>
2018-03-05 22:13:49 -0500

    added commands

---
+++ src/commandNode/Turtles.java
@@ -0,0 +1,27 @@
+package commandNode;
+
+import java.util.List;
+
+import model.Model;
+import nodes.CommandInterface;
+import nodes.GeneralCommand;
+import nodes.NodeInterface;
+
+public class Turtles extends GeneralCommand implements CommandInterface{
+
+	public Turtles(Model model, int numChildren) {
+		super(model, numChildren);
+	}
+
+	@Override
+	/**
+	 * returns number of turtles created so far
+	 * 
+	 * @return num turtles
+	 */
+	public double evaluate(List<NodeInterface> arguments) {
+		value = model.getAllTurtles().size();
+		return value;
+	}
+
+}
